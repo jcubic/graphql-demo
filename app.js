c027ab74a31aae5a8626361506ec7a228b9878fd
Jakub Jankiewicz <jcubic@onet.pl>
2022-09-20 22:00:44 +0200

    restructure for Atthost

--- app.js
+++ app.js
@@ -1,24 +1,29 @@
 import { ApolloServer, gql } from 'apollo-server';
+import fs from 'node:fs';
+
 
 const typeDefs = gql`
   type Query {
     greeting: String
     names: [String]
+    lorem: [String]
   }
 `;
 
 const data = {
   greeting: "Hello, world!",
-  names: ['lorem', 'ipsum', 'dolor', 'sit', 'amet']
+  names: ['lorem', 'ipsum', 'dolor', 'sit', 'amet'],
+  lorem: fs.readFileSync('lorem.txt', 'utf8').split(/\n+/).filter(Boolean)
 };
 
 const server = new ApolloServer({
   typeDefs,
-  rootValue: data
+  rootValue: data,
+  introspection: true
 });
 
 server.listen({
-  port: 4000
+  port: 3000
 }).then(result => {
   console.log(`Listening on ${result.url}`);
 });
