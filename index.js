acdc4646cc6ae49c8e5a4827c0ebbbbffdd6e7b0
Jakub Jankiewicz <jcubic@onet.pl>
2022-09-20 20:42:53 +0200

    simple GraphQL server

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log('hello, world!');
+import { ApolloServer } from 'apollo-server';
+
+const typeDefs = `
+  type Query {
+    greeting: String
+  }
+`;
+
+const server = new ApolloServer({
+  typeDefs
+});
+
+server.listen({
+  port: 4000
+}).then(result => {
+  console.log(`Listening on ${result.url}`);
+});
